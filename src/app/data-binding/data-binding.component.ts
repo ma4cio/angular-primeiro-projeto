453a4625c934ede0132f777459db8dd9078220c5
Marcio Lima <marcio.lima@bancodaycoval.com.br>
2022-06-08 18:34:42 -0300

    Aula Property Binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -9,7 +9,11 @@ export class DataBindingComponent implements OnInit {
 
   public nome: string = "Marcio Lima";
   public idade: number = 42;
-  
+  public checkedDisabled: boolean = false;
+  public imgSrc: string = "https://i0.wp.com/nightwolf.dev/wp-content/uploads/angular.png?resize=150%2C150&ssl=1";
+  public altSrc: string = "Property Binding";
+  public titleSrc: string = "Property Binding";
+
   constructor() { }
 
   ngOnInit(): void {
